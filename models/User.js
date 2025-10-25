4517e6f51857f33174fe6cec9d9814aa2ac7c5ee
manzxy <141548199+manzxy@users.noreply.github.com>
2025-10-26 00:57:07 +0700

    Create User model with schema definition

---
+++ models/User.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  uid: { type: String, unique: true, required: true },
+  provider: String,
+  name: String,
+  email: String,
+  picture: String,
+  password: String,
+  commentsCount: { type: Number, default: 0 },
+  title: { type: String, default: '🐣 Pembaca Baru' },
+  role: { type: String, default: 'user' }, // 'user' or 'admin'
+  lastLoginIP: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
